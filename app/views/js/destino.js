0b4721ab5628ed80c2042732de25818d5c520cf5
Eduardo Gabriel Molino <egmprog@gmail.com>
2024-05-12 18:58:49 -0300

    20240512B

--- app/views/js/destino.js
+++ app/views/js/destino.js
@@ -1,11 +1,27 @@
-const selectElement = document.getElementById('destino');
-const submitButton = document.querySelector('input[type="submit"]');
-
-submitButton.addEventListener('click', function () {
-    const selectedOption = selectElement.options[selectElement.selectedIndex];
-    const selectedValue = selectedOption.value;
-    const selectedText = selectedOption.textContent;
-
-    console.log('Valor seleccionado:', selectedValue);
-    console.log('Texto seleccionado:', selectedText);
-});
+<script>
+    function redirect() {
+        // Obtener el valor seleccionado en el menú desplegable
+        var destino = document.getElementById("destino").value;
+        
+        // Redirigir según la opción seleccionada
+        switch (destino) {
+            case "norte":
+                window.location.href = "/app/views/content/norte.html";
+                break;
+            case "sur":
+                window.location.href = "/app/views/content/sur.html";
+                break;
+            case "cordoba":
+                window.location.href = "/app/views/content/cordoba.html";
+                break;
+            case "mardel":
+                window.location.href = "/app/views/content/mardel.html";
+                break;
+            default:
+                // Redirigir a una página predeterminada si no se seleccionó ninguna opción válida
+                window.location.href = "/app/views/content/offers.html";
+        }
+        // Evitar que se envíe el formulario normalmente
+        return false;
+    }
+</script>
